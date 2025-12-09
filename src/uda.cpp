c3f11f9ba315811e94cc1d67623e9478fc5cf48f
tryingtothink <gammbol2525@gmail.com>
2025-12-09 16:53:15 +0300

    added routes

--- src/uda.cpp
+++ src/uda.cpp
@@ -15,19 +15,16 @@ int main()
         "3050"
     };
 
+    // std::cout << "Connecting to Firebird database..." << std::endl;
+    // if (!fbc.connect()) {
+    //     std::cerr << "FATAL: Failed to connect to database on startup!" << std::endl;
+    //     return 1; // Завершаем приложение, если не удалось подключиться
+    // }
+    // std::cout << "Successfully connected to database!" << std::endl;
 
-
-    FirebirdConnection fbc(fb_conf);
-
-    std::cout << "Connecting to Firebird database..." << std::endl;
-    if (!fbc.connect()) {
-        std::cerr << "FATAL: Failed to connect to database on startup!" << std::endl;
-        return 1; // Завершаем приложение, если не удалось подключиться
-    }
-    std::cout << "Successfully connected to database!" << std::endl;
-
-    CROW_ROUTE(app, "/api/boards")([&fbc]() {
+    CROW_ROUTE(app, "/api/boards")([&fb_conf]() {
         try {
+            FirebirdConnection fbc(fb_conf);
 
             // Получаем данные из БД
             std::vector<crow::json::wvalue> boards = fbc.getSQL("SELECT * FROM DEVICES");
@@ -37,7 +34,6 @@ int main()
             for (size_t i = 0; i < boards.size(); i++) {
                 result_json[i] = std::move(boards[i]);
             }
-            std::cout << result_json.dump() << std::endl;
 
             // Возвращаем JSON напрямую - Crow знает как его обработать
             return crow::response(200, result_json);
@@ -46,6 +42,88 @@ int main()
             std::cerr << "Error in /api/boards: " << e.what() << std::endl;
 
             return crow::response(500, "pizdec :(");
+        }
+    });
+
+    CROW_ROUTE(app, "/api/boards/<int>")([&fb_conf](int id) {
+        try {
+            FirebirdConnection fbc(fb_conf);
+
+            std::string query = "SELECT * FROM DEVICES WHERE DEVICE_ID=" + std::to_string(id);
+            std::cout << query << std::endl;
+
+            std::vector<crow::json::wvalue> board = fbc.getSQL(query);
+            // crow::json::wvalue result_json;
+            // result_json[0] = std::move(board[0]);
+
+            return crow::response(200, board[0]);
+        } catch (const std::exception &e) {
+            std::cerr << "Error in /api/boards: " << e.what() << std::endl;
+            return crow::response(500, e.what());
+        }
+    });
+
+    CROW_ROUTE(app, "/api/sessions/<int>")([&fb_conf](int id) {
+        try {
+            FirebirdConnection fbc(fb_conf);
+
+            std::string query = "SELECT * FROM RD2_SESSIONS WHERE DEVICE_ID=" + std::to_string(id) + " ORDER BY SESSION_ID DESC";
+            std::cout << query << std::endl;
+
+            std::vector<crow::json::wvalue> sessions = fbc.getSQL(query);
+
+            crow::json::wvalue result_json;
+            for (size_t i = 0; i < sessions.size(); i++) {
+                result_json[i] = std::move(sessions[i]);
+            }
+
+            return crow::response(200, result_json);
+        } catch (const std::exception &e) {
+            std::cerr << "Error in /api/sessions: " << e.what() << std::endl;
+
+            return crow::response(500, e.what());
+        }
+    });
+
+    CROW_ROUTE(app, "/api/session/<int>")([&fb_conf](int id) {
+        try {
+            FirebirdConnection fbc(fb_conf);
+
+            std::string query = "SELECT * FROM RD2_SESSIONS WHERE SESSION_ID=" + std::to_string(id);
+            std::cout << query << std::endl;
+
+            std::vector<crow::json::wvalue> session = fbc.getSQL(query);
+
+            // crow::json::wvalue result_json;
+            // for (size_t i = 0; i < session.size(); i++) {
+            //     result_json[i] = std::move(session[i]);
+            // }
+
+            return crow::response(200, session[0]);
+        } catch (const std::exception &e) {
+            std::cerr << "Error in /api/session: " << e.what() << std::endl;
+            return crow::response(500, e.what());
+        }
+    });
+
+    CROW_ROUTE(app, "/api/points/<int>")([&fb_conf](int id) {
+        try {
+            FirebirdConnection fbc(fb_conf);
+
+            std::string query = "SELECT * FROM RD2_POINTS WHERE SESSION_ID=" + std::to_string(id);
+            std::cout << query << std::endl;
+
+            std::vector<crow::json::wvalue> points = fbc.getSQL(query);
+
+            crow::json::wvalue result_json;
+            for (size_t i = 0; i < points.size(); i++) {
+                result_json[i] = std::move(points[i]);
+            }
+
+            return crow::response(200, result_json);
+        } catch (const std::exception &e) {
+            std::cerr << "Error in /api/points: " << e.what() << std::endl;
+            return crow::response(500, e.what());
         }
     });
 
